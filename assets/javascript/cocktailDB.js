5396ce67336b95707cc5506471bad8a7f2bfea45
Kent Schollmeier <jschollmeier@gmail.com>
2020-06-24 16:12:32 -0500

    added modal functionality to cocktailDB and added modal framework to index.html

--- assets/javascript/cocktailDB.js
+++ assets/javascript/cocktailDB.js
@@ -3,12 +3,14 @@ lookupByIngredient("vodka");
 var drinks = [];
 
 function displayButtons(drinks){
+  
   for (let index = 0; index < 10; index++) {
-    
+    //console.log("this happened")
     var addDrinks = $("<button>")
     addDrinks.addClass("drink-btn")
     addDrinks.attr("drinkID", drinks[index].idDrink);
     addDrinks.attr("index", index);
+   // addDrinks.data('target', '#modelId')
     var imgs = $("<img src="+ drinks[index].strDrinkThumb+">")
     var text = $("<div>").text(drinks[index].strDrink)
     addDrinks.append(text,imgs);
@@ -38,15 +40,17 @@ $.ajax({
         
         }
         displayButtons(drinks);
-        console.log(drinks);
+        //console.log(drinks);
   })
 }
 
 $(document).on('click', ".drink-btn",function(event){
     event.preventDefault()
-    $(".test").empty()
+    $(".modal-body").empty()
+    //$(".test").empty()
     var testID = $(this).attr("drinkID")
     var indexSpot = $(this).attr("index")
+    $("#modelId").modal("show");
 
     //console.log(testID)
     lookupDrink(testID, indexSpot)
@@ -64,34 +68,36 @@ $.ajax({
   }).then(function(ingredientPull) {
     var i = 1;
     var name = "strIngredient"+i;
-    var addPic = $("<div>");
+    //var addPic = $("<div>");
     var imgs1 = $("<img src="+ drinks[indx].strDrinkThumb+">")
     var text1 = $("<div>").text(drinks[indx].strDrink)
-    addPic.append(text1,imgs1);
-    $(".test").append(addPic);
+    //addPic.append(text1,imgs1);
+    //console.log(drinks[indx].strDrink)
+   $(".modal-title").text(drinks[indx].strDrink)
+   $(".modal-body").append(imgs1);
     
     while(ingredientPull.drinks[0][name] != null) {
             var list = $("<ul>")
             var ingredients = $('<li>').text(ingredientPull.drinks[0][name]);
             i++;
             name = "strIngredient"+i;
-            $(".test").append(ingredients)
+            $(".modal-body").append(ingredients)
        }
 
        var instructions = $("<div>").text(ingredientPull.drinks[0].strInstructions)
-       $(".test").append(instructions);
+       $(".modal-body").append(instructions);
       //console.log(drinks)
-      
+    /*  
     var newBtn = $("<button>");
     newBtn.text("Go Back");
     newBtn.addClass("back-btn")
     $(".test").append(newBtn);
-
+*/
        //console.log(ingredientPull)
     })
   }
 
   $(document).on('click', ".back-btn",function(event){
-    $(".test").empty();
+    //$(".test").empty();
     displayButtons(drinks);
 })
