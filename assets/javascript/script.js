bb35fd9553ef922ecce00536810a7db8dee5a989
unknown <davidhale87@gmail.com>
2020-06-24 08:07:11 -0500

    Adding the recipe pull and link.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,37 +17,48 @@ for (let i = 0; i <= 1; i++) {
 }
 
 var recipeID
+var addLink
+var pick = 2
   function websiteCall(){
     $.ajax({
       url: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + Ingr + "&apiKey=2b49753a505a43fe8dbfb610bb43e250",
       method: "GET"
     }).then(function(data) {
       console.log(data)
-      console.log(recipeID = data[0].id)
-      recipeID = data[0].id
-      callRecipe()
-
+      console.log(recipeID = data[pick].id)
+      recipeID = data[pick].id
+      callRecipeURL()
     }); 
   }
 
-  function callRecipe (){
+function callRecipeURL (){
   $.ajax({
     url: "https://api.spoonacular.com/recipes/" + recipeID +"/information?includeNutrition=false&apiKey=2b49753a505a43fe8dbfb610bb43e250",
     method: "GET"
   }).then(function(data) {
-    var addLink = $('<a>').attr('href',data.sourceUrl).text(data.sourceName)
+    $.ajax({
+      url: "https://api.spoonacular.com/recipes/extract?" + data.sourceUrl+"&apiKey=2b49753a505a43fe8dbfb610bb43e250",
+      method: "GET"
+    }).then(function(extract) {
+      var extract = $('<div>').text(extract)
+      console.log(extract)
+    });
+    addLink = $('<a>').attr('href',data.sourceUrl).text(data.title)
     var image = $('<img>').attr('src',data.image)
     $('#Test').append(addLink,image)
-    
-    
     console.log("Test and Recipe ID " + recipeID)
-
-
     console.log(data)
-    
-    
-
   }); 
 }
+  function extractRecipe(){
+    $.ajax({
+      url: "https://api.spoonacular.com/recipes/extract?" + data.sourceUrl+"&apiKey=2b49753a505a43fe8dbfb610bb43e250",
+      method: "GET"
+    }).then(function(extract) {
+      var extract = $('<div>').text(extract)
+      console.log(extract)
+    }); 
+  }
+
 
 websiteCall()
