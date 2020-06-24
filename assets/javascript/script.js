134f96ec79aed68b70234868cd0e6fd098ff75e9
unknown <davidhale87@gmail.com>
2020-06-23 23:39:45 -0500

    added another ajax call to pull the actual recipe up. We have to access it by the recipe ID I think. Then I just am creating a basic link to click on and the image associated with the link.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ var userIngr = ''
 var Ingr = ''
 
 
-for (let i = 0; i <= 3; i++) {
+for (let i = 0; i <= 1; i++) {
   userIngr = prompt()
     if(Ingr !== ''){
       Ingr += ',+' + userIngr
@@ -16,17 +16,38 @@ for (let i = 0; i <= 3; i++) {
   console.log(Ingr)
 }
 
-
+var recipeID
   function websiteCall(){
     $.ajax({
       url: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + Ingr + "&apiKey=2b49753a505a43fe8dbfb610bb43e250",
       method: "GET"
     }).then(function(data) {
       console.log(data)
-      
-
+      console.log(recipeID = data[0].id)
+      recipeID = data[0].id
+      callRecipe()
 
     }); 
   }
+
+  function callRecipe (){
+  $.ajax({
+    url: "https://api.spoonacular.com/recipes/" + recipeID +"/information?includeNutrition=false&apiKey=2b49753a505a43fe8dbfb610bb43e250",
+    method: "GET"
+  }).then(function(data) {
+    var addLink = $('<a>').attr('href',data.sourceUrl).text(data.sourceName)
+    var image = $('<img>').attr('src',data.image)
+    $('#Test').append(addLink,image)
+    
+    
+    console.log("Test and Recipe ID " + recipeID)
+
+
+    console.log(data)
+    
+    
+
+  }); 
+}
 
 websiteCall()
