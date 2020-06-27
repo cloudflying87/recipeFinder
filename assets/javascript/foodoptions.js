6f090f9cb7d92f657a92f3336ea50e9f47479dae
David Hale <davidhale87@gmail.com>
2020-06-27 07:57:22 -0500

    cleaning up the code some.

--- assets/javascript/foodoptions.js
+++ assets/javascript/foodoptions.js
@@ -2,30 +2,6 @@
 
 https://api.spoonacular.com/recipes/search?query=cheese&number=2&diet=vegetarian&exlcudeIngredients=eggs&intolerances=gluten&apiKey=0dcf6018121d4ae3ab90ebb53ead0081 
 */
-
-var foodCategory = ['Bread', 'Rice', 'Cheese', 'Egg', 'Fish','Chicken', 'Prawn', 'Sugar', 'Potato', 'Mango'] 
-
-var drinkCategory = ['gin','vodka','rum']
-
-categoryCall(foodCategory,".foodOptionsList");
-categoryCall(drinkCategory,".drinkOptions");
-
-function categoryCall(category,appendPlace) {
-    
-    for (let index= 0; index < category.length; index++) {
-        var NewDiv = $('<div>').addClass("form-check form-check-inline")
-        var NewInput = $('<input>').addClass("form-check-input")
-        NewInput.attr('id', category[index])
-        NewInput.attr('type','checkbox')
-        
-        var NewLabel = $('<label>').addClass('form-check-label').text(category[index])
-        NewDiv.append(NewInput,NewLabel)
-        $(appendPlace).append(NewDiv)   
-    }
-}
-
-var menuClicked;
-
 var displayIntolerances = ['Dairy','Egg','Gluten','Grain','Peanut','Seafood','Sesame','Shellfish','Soy','Sulfite','Tree','Nut','Wheat']
 
 var intolerances = ['dairy','egg','gluten','grain','peanut','seafood','sesame','shellfish','soy','sulfite','tree','nut','wheat']
@@ -34,166 +10,50 @@ var displayDietRestrictions = ['Gluten Free','Ketogenic','Vegetarian','Lacto-Veg
 
 var dietrestrictions = ['glutenfree','ketogenic','vegetarian','lacto-vegetarian','ovo-vegetarian','vegan','pescetarian','paleo','primal','whole30']
 
-addingOptions(intolerances,displayIntolerances,'.intolerances');
-addingOptions(dietrestrictions,displayDietRestrictions,'.diet');
+var foodCategory = ['Bread', 'Rice', 'Cheese', 'Egg', 'Fish','Chicken', 'Prawn', 'Sugar', 'Potato', 'Mango'] 
 
-function addingOptions(selectOptions,displayOptions,placement){
-    for (let i = 0; i < selectOptions.length; i++) {
-    var list = $("<input>");
-    list.attr('type','checkbox')
-    list.val(selectOptions[i]).text(displayOptions[i]);
-    $(placement).append(list) 
-}};
+var drinkCategory = ['gin','vodka','rum']
 
-function toggleClass(elem,className){
-    if (elem.className.indexOf(className) !== -1){
-      elem.className = elem.className.replace(className,'');
-    }
-    else{
-      elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-    }
-    return elem;
-  }
-  
-  function toggleDisplay(elem){
-    const curDisplayStyle = elem.style.display;			
-  
-    if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-      elem.style.display = 'block';
-    }
-    else{
-      elem.style.display = 'none';
-    }
-  }
-  
-  function toggleMenuDisplay(e){
-    const dropdown = e.currentTarget.parentNode;
-    const menu = dropdown.querySelector('.menu');
-    const icon = dropdown.querySelector('.fa-angle-right');
-  
-    toggleClass(menu,'hide');
-    toggleClass(icon,'rotate-90');
-  }
-  
-  function handleOptionSelected(e){
-    toggleClass(e.target.parentNode, 'hide');			
-  
-    const id = e.target.id;
-    const newValue = e.target.textContent + ' ';
-    const titleElem = document.querySelector('.dropdown .title');
-    const icon = document.querySelector('.dropdown .title .fa');
-  
-  
-    titleElem.textContent = newValue;
-    titleElem.appendChild(icon);
-  
-    //trigger custom event
-    document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-      //setTimeout is used so transition is properly shown
-    setTimeout(() => toggleClass(icon,'rotate-90',0));
-  }
-  
-  function handleTitleChange(e){
-    const result = document.getElementById('result');
-  
-    console.log(e.target.textContent);
-  }
-  
-  //get elements
-  const dropdownTitle = document.querySelector('.dropdown .title');
-  const dropdownOptions = document.querySelectorAll('.dropdown .option');
-  
-  //bind listeners to these elements
-  dropdownTitle.addEventListener('click', toggleMenuDisplay);
-  
-  dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-  
-  document.querySelector('.dropdown .title').addEventListener('change',handleTitleChange);
+categoryCall(foodCategory,foodCategory,".foodOptionsList");
+categoryCall(drinkCategory,drinkCategory,".drinkOptions");
 
-  // var checkList = document.getElementById('list1');
-//             checkList.$('.anchor')[0].onclick = function (evt) {
-//                 if (checkList.classList.contains('visible'))
-//                     checkList.classList.remove('visible');
-//                 else
-//                     checkList.classList.add('visible');
-//             }
+// addingOptions(dietrestrictions,displayDietRestrictions,'.diet');
+
+function categoryCall(category,categoryDisplay,appendPlace) {
     
-//             checkList.onblur = function(evt) {
-//                 checkList.classList.remove('visible');
-//             }
+    for (let index= 0; index < category.length; index++) {
+        var NewDiv = $('<div>').addClass("form-check form-check-inline")
+        var NewInput = $('<input>').addClass("form-check-input")
+        NewInput.attr('id', category[index])
+        NewInput.attr('type','checkbox')
+        
+        var NewLabel = $('<label>').addClass('form-check-label').text(categoryDisplay[index])
+        NewDiv.append(NewInput,NewLabel)
+        $(appendPlace).append(NewDiv)   
+    }
+}
 
-// tippy('#myButton', {
-//     allowHTML: true,
-//     animateFill: true,
-//     animation: 'scale',
-//     animation: 'shift-away',
-//     content: messages,
-// })
-/*
-function toggleClass(elem,className){
-if (elem.className.indexOf(className) !== -1){
-    elem.className = elem.className.replace(className,'');
-}
-else{
-    elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-}
-return elem;
-}
-var checkList = document.getElementById('list1');
-            checkList.$('.anchor')[0].onclick = function (evt) {
-                if (checkList.classList.contains('visible'))
-                    checkList.classList.remove('visible');
-                else
-                    checkList.classList.add('visible');
-            }
+writeSelect(intolerances,displayIntolerances,'#intolerances');
 
-            checkList.onblur = function(evt) {
-                checkList.classList.remove('visible');
-            }
-function toggleDisplay(elem){
-const curDisplayStyle = elem.style.display;			
-if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-    elem.style.display = 'block';
+function writeSelect(category,categoryDisplay,appendPlace) {
+    
+  for (let index= 0; index < category.length; index++) {
+      var newOption = $('<option>').attr('value',category[index]).text(categoryDisplay[index])
+      $(appendPlace).append(newOption)
+  }
 }
-else{
-    elem.style.display = 'none';
+new SlimSelect({
+  select: '#intolerances',
+  showSearch: false,
+  onChange: (data) => {
+    workingDropdown(data)},
+})
+var intoleranceSelection = ''
+function workingDropdown(data){
+  for (let i = 0; i < data.length; i++) {
+    intoleranceSelection = (data[i].value)  
+    console.log(intoleranceSelection)
+  }
+  
 }
-}
-function toggleMenuDisplay(e){
-// const dropdown = e.currentTarget.parentNode
-console.log(e)
-const menu = dropdown.querySelector('.menu');
-const icon = dropdown.querySelector('.fa-angle-right');
-toggleClass(menu,'hide');
-toggleClass(icon,'rotate-90');
-}
-function handleOptionSelected(e){
-toggleClass(e.target.parentNode, 'hide');			
-const id = e.target.id;
-const newValue = e.target.textContent + ' ';
-const titleElem = document.querySelector('.dropdown .title');
-const icon = document.querySelector('.dropdown .title .fa');
-titleElem.textContent = newValue;
-titleElem.appendChild(icon);
-//trigger custom event
-document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-    //setTimeout is used so transition is properly shown
-setTimeout(() => toggleClass(icon,'rotate-90',0));
-}
-function handleTitleChange(e){
-const result = document.getElementById('result');
-console.log(e.target.textContent)
-}
-//get elements
-const dropdownTitle = document.querySelector('.dropdown .title');
-const dropdownOptions = document.querySelectorAll('.dropdown .option');
-//bind listeners to these elements
-$('.dropdown').on('click', function(event){
-    event.preventDefault();
-    menuClicked = this.id
-    toggleMenuDisplay(menuClicked);
-}
-);
-dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-document.querySelector('.dropdown .title').addEventListener('change',handleTitleChange);
-*/
+
--- assets/javascript/spoonacular.js
+++ assets/javascript/spoonacular.js
@@ -4,9 +4,11 @@ var addLink
 var food = []
 var inc = 0
 var hmm = 1;
+var dietSelect =''
+var intoleranceSelect = ''
+var excludeSelect = ''
 
 $("#ingrBt").click(function(){
-  //console.log('button clicked')
   event.preventDefault()
   var item = $("#ingrInpt").val().trim()
       if(Ingr !== ''){
@@ -15,14 +17,12 @@ $("#ingrBt").click(function(){
       else{
         Ingr += item
       }
-    //console.log(Ingr)
 renderFood(item)
 }); 
 
 function openCity(cityName) {
   var i;
   var x = document.getElementsByClassName("options");
-  //console.log(x);
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
@@ -32,7 +32,6 @@ function openCity(cityName) {
 function openMenu(menuName) {
   var i;
   var x = document.getElementsByClassName("options1");
- // console.log(x);
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
@@ -55,61 +54,38 @@ function renderFood(text){
   $("#ingrList").append(a);
   
   websiteCall(Ingr);
- // console.log(inc)
-
 };
 
 $(document).on('click','.delete', function(event){
   var testy = $(".ingrItem-"+$(this).attr("ind")).text();
-  
   var be = $(this).attr("ind");
   this.remove();
   $(".ingrItem-"+$(this).attr("ind")).remove();
-
-  //console.log(be+"    "+hmm)
-
   if (Ingr.indexOf('+')>-1)
   {
     if(be==hmm){
       Ingr = Ingr.replace(testy+",+", "");
-      console.log("happening");
       hmm++;
-
     }
     else{
       Ingr = Ingr.replace(',+'+testy, "");
     }
-    
-    
   }
   else{
     Ingr = Ingr.replace(testy, "");
-    
   }
-  
- // console.log(Ingr);
  $(".recipe").empty();
-  websiteCall(Ingr);
-
-  
-  
-  
+  websiteCall(Ingr)
 
 });
 
-// window.onbeforeunload = function() {
-//   return "Dude, are you sure you want to leave? Think of the kittens!";
-//  }
-
 // initial call to the api based on the users search ingredients. 
 function websiteCall(url){
-  //console.log("why arent I working")
     $.ajax({
-      url: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + url + "&apiKey=2b49753a505a43fe8dbfb610bb43e250",
+      url: "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + url + "&diet=" + dietSelect + "&exlcudeIngredients="+ excludeSelect +"&intolerances="+ intoleranceSelect +"&apiKey=2b49753a505a43fe8dbfb610bb43e250",
       method: "GET"
     }).then(function(initialPull){
       food = [];
-      console.log("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + url + "&apiKey=2b49753a505a43fe8dbfb610bb43e250");
       for (let i = 0; i < initialPull.length; i++) {
         food.push(initialPull[i]) 
       }
@@ -117,60 +93,42 @@ function websiteCall(url){
     }); 
   }
 
-  function displayChoices(foods){
-     
-    for (let index = 0; index < 12; index++) {
-      
-  
-      var newDiv = $("<button>").addClass("foodOptions button col-lg-3 col-md-4 col-6");
-      newDiv.attr("recipeID", foods[index].id);
-      //console.log(food[index].id)
-      newDiv.css({"padding": "10px", "border": "none","margin":"10px","margin-bottom":"20px", });
-  
-      var newText = $("<p>").addClass("text-danger");
-      newText.text(foods[index].title);
-      newText.css({"text-align":"center","line-height":"0%"});
-      
-      if (foods[index].title.length > 20 &&foods[index].title.length < 40){
-        newText.css("font-size","small")
-        newText.css("line-height","100%")
-      }
-      else if (foods[index].title.length <25){
-       //console.log("this happens")
-        
-      }
-      else{
-        newText.css("line-height","100%")
-       // console.log("this")
-      }
-      var newLink = $("<a>").addClass("d-block mb-4 h-100");
-      
-      
-      var newImg = $("<img src="+ foods[index].image +">").addClass("img-fluid img-thumbnail");
-      newImg.css("border","none")
-      //newImg.css({"margin-top":"0px","padding-top":"0px","border-top":"0px"})
-      newLink.append(newImg);
-      newDiv.append(newText,newLink);
-      $(".recipe").append(newDiv);
+function displayChoices(foods){
+  for (let index = 0; index < 12; index++) {
+
+    var newDiv = $("<button>").addClass("foodOptions button col-lg-3 col-md-4 col-6");
+    newDiv.attr("recipeID", foods[index].id);
+    newDiv.css({"padding": "10px", "border": "none","margin":"10px","margin-bottom":"20px", });
+
+    var newText = $("<p>").addClass("text-danger");
+    newText.text(foods[index].title);
+    newText.css({"text-align":"center","line-height":"0%"});
+    
+    if (foods[index].title.length > 20 &&foods[index].title.length < 40){
+      newText.css("font-size","small")
+      newText.css("line-height","100%")
     }
-    /*for (let i = 0; i < 9; i++) {
-        var addRecipeOptions = $('<button>');
-        addRecipeOptions.addClass('foodOptions button col-lg-3 col-md-4 col-6');
-        addRecipeOptions.attr('recipeID',food[i].id);
-        var imgFood = $("<img src="+ food[i].image +">") ;
-        var text = $('<p>').text(food[i].title);
-        text.addClass("text-danger")
-        addRecipeOptions.append(text,imgFood);
-        $(".recipe").append(addRecipeOptions)  
-      }  */
+    else if (foods[index].title.length <25){
+    }
+    else{
+      newText.css("line-height","100%")
+    }
+    var newLink = $("<a>").addClass("d-block mb-4 h-100");
+    
+    var newImg = $("<img src="+ foods[index].image +">").addClass("img-fluid img-thumbnail");
+    newImg.css("border","none")
+    newLink.append(newImg);
+    newDiv.append(newText,newLink);
+    $(".recipe").append(newDiv);
   }
+  
+}
   // calling the recipe url to get the instructions
 function callRecipeURL (recipeID){
   $.ajax({
     url: "https://api.spoonacular.com/recipes/" + recipeID +"/information?includeNutrition=false&apiKey=2b49753a505a43fe8dbfb610bb43e250",
     method: "GET"
   }).then(function(data) { 
-    //console.log(data.sourceUrl);
     window.open(data.sourceUrl, '_blank') 
   }); 
 }
@@ -180,15 +138,3 @@ $(document).on('click','.foodOptions', function(event){
   var testID = $(this).attr('recipeID');
   callRecipeURL(testID);
 })
-
-/* if we want to try extract the recipe from the site instead of just linking to the site.
-  function extractRecipe(){
-    $.ajax({
-      url: "https://api.spoonacular.com/recipes/extract?" + data.sourceUrl+"&apiKey=2b49753a505a43fe8dbfb610bb43e250",
-      method: "GET"
-    }).then(function(extract) {
-      var extract = $('<div>').text(extract)
-      console.log(extract)
-    }); 
-  }
-*/
